667440d2c95701498081e2aba2124b719b57f589
zaidi0069 <93919683+zaidi0069@users.noreply.github.com>
2024-02-13 11:19:51 +0500

    subtraction code

--- app.py
+++ app.py
@@ -6,3 +6,16 @@ num1 = 5
 num2 = 3
 result = add_numbers(num1, num2)
 print("The sum of", num1, "and", num2, "is:", result)
+
+
+
+def subtract_numbers(a, b):
+    return a - b
+
+# Example usage:
+num1 = 5
+num2 = 3
+result = subtract_numbers(num1, num2)
+print("The diff of", num1, "and", num2, "is:", result)
+
+
